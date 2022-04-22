ad4b13f7297ddbc72a524ff0625cf085ad7b38b5
yousub <yousub@socialbean.co.kr>
2022-04-22 15:37:01 +0900

    first commit

---
+++ html/_layout/_header.js
@@ -0,0 +1,54 @@
+var str =
+`<div class="header">
+  <h1>
+      <a href="../../html/main/index.html"><img src="../../img/main/vcs-logo-white.png" alt=""></a>
+  </h1>
+  <input class="burger-check" type="checkbox" id="burger-check" />
+  <nav class="nav">
+    <ul class="menu">
+      <li>
+        <a her="">3x3x3</a>
+        <ul class="menu-sub">
+          <li><a href="../../html/sub/three_f2l.html">F2L</a></li>
+          <li><a href="../../html/sub/three_oll.html">OLL</a></li>
+          <li><a href="../../html/sub/three_pll.html">PLL</a></li>
+        </ul>
+      </li>
+      <li>
+        <a her="">2x2x2</a>
+        <ul class="menu-sub">
+          <li><a href="../../html/sub/two_ortega.html">Ortega</a></li>
+          <li><a href="../../html/sub/two_eg.html">EG</a></li>
+        </ul>
+      </li>
+      <li>
+        <a her="">Big cube</a>
+        <ul class="menu-sub">
+          <li><a href="../../html/sub/big_4x4x4.html">4x4x4</a></li>
+          <li><a href="../../html/sub/big_5x5x5.html">5x5x5</a></li>
+          <li><a href="../../html/sub/big_6x6x6.html">6x6x6</a></li>
+          <li><a href="../../html/sub/big_7x7x7.html">7x7x7</a></li>
+        </ul>
+      </li>
+      <li>
+        <a her="">Megaminx</a>
+        <ul class="menu-sub">
+          <li><a href="../../html/sub/megaminx_2look.html">2look</a></li>
+          <li><a href="../../html/sub/megaminx_oll.html">OLL</a></li>
+          <li><a href="../../html/sub/megaminx_pll.html">PLL</a></li>
+        </ul>
+      </li>
+    </ul>
+  </nav>    
+  <div>
+  </div>    
+  <label class="burger-icon" for="burger-check">
+    <span class="burger-sticks"></span>
+  </label>
+  <div class="menu-burger">
+    <div style="width: 200px;"></div>
+  </div>
+</div>
+`;
+
+document.write(str);
